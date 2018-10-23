d7e54e91054d4a547ec21efad3144738821a3d81
hectorgonzsg <44251881+hectorgonzsg@users.noreply.github.com>
2018-10-23 12:07:52 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,13 +1,36 @@
+<?php
 
-<!DOCTYPE html>
-<html>
-    <head>
-        
-    </head>
-    <body>
-        <form action="uploadfile.php" enctype="multipart/form-data"  method="post">
-            <input type="file" multiple name="file[]"/>
-            <input type="submit" name="submit" value="Submit"/>
-        </form>
-    </body>
-</html>
+class upload{
+    
+ private $formatos = array('.jpg','.pgn');
+ 
+ private       $file = 'archivo',
+        $boton = 'submit';
+ function comprobar($file,$boton){
+     $num_archivos= count($_FILES['file']['name']);
+     echo $num_archivos;
+     for($i = 0;$i<$num_archivos;$i++){
+             $nombreArchivo = $_FILES[$file]['name'][$i];
+             $nombreTmpArchivo = $_FILES[$file]['tmp_name'][$i];
+             $ext = substr($nombreArchivo,strpos($nombreArchivo,'.'));
+             echo $ext;
+             if(in_array($ext, $this->formatos)){
+                 if(move_uploaded_file($nombreTmpArchivo, $nombreArchivo)){
+                 echo "FELICIDADES";
+                 
+             }else{
+                 
+             
+         }}else {
+             echo "no archivo permitido";
+             echo "a" . $ext;
+         }
+}
+}
+}
+
+/*if(isset($_FILE['file']) && $_FILES[$input]['error'] === 0 && $_FILES[$input]['name'] != '') {
+    $file = $_FILE['file'];
+    $this->name=$this->file['name'];
+}
+*/
